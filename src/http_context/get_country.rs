ef3c59363f2d016ee9f22e88d213b1683584be34
Maxim Gorin <maksim.g@simple.app>
2022-12-14 19:27:55 +0300

    Fix getting country

--- src/http_context/get_country.rs
+++ src/http_context/get_country.rs
@@ -8,8 +8,8 @@ pub trait GetCountry {
 
 impl GetCountry for HttpContext {
     fn get_country_alpha2(&self) -> Result<String, HttpFailResult> {
-        if let Some(header_value) = self.request.get_key_value(HEADER) {
-            if let Ok(parsed_header_value) = std::str::from_utf8(header_value) {
+        if let Some(header_value) = self.request.get_headers().get(HEADER) {
+            if let Ok(parsed_header_value) = std::str::from_utf8(header_value.as_bytes()) {
                 return Ok(parsed_header_value.to_owned());
             }
         }
