7fefeeec5c65e7e0bff1fb351adc987663144bdd
Maxim Gorin <maksim.g@simple.app>
2022-12-14 16:13:54 +0300

    Add method for getting country

---
+++ src/http_context/get_country.rs
@@ -0,0 +1,19 @@
+use my_http_server::{HttpContext, HttpFailResult};
+
+const HEADER: &str = "cf-ipcountry";
+
+pub trait GetCountry {
+    fn get_country_alpha2(&self) -> Result<String, HttpFailResult>;
+}
+
+impl GetCountry for HttpContext {
+    fn get_country_alpha2(&self) -> Result<String, HttpFailResult> {
+        if let Some(header_value) = self.request.get_key_value(HEADER) {
+            if let Ok(parsed_header_value) = std::str::from_utf8(header_value) {
+                return Ok(parsed_header_value.to_owned());
+            }
+        }
+
+        return Ok("".to_string());
+    }
+}
--- src/http_context/mod.rs
+++ src/http_context/mod.rs
@@ -1,7 +1,9 @@
 mod get_client_id;
 mod get_lang;
 mod get_brand_id;
+mod get_country;
 
 pub use get_client_id::*;
 pub use get_lang::*;
 pub use get_brand_id::*;
+pub use get_country::*;
