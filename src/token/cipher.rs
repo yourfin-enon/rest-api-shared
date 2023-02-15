053f27da75cbfe5655d194e77fdb25dc60d70b20
Maxim Gorin <maksim.g@simple.app>
2023-02-15 12:46:06 +0300

    Remove redundant copy on encrypt; Refactoring

--- src/token/cipher.rs
+++ src/token/cipher.rs
@@ -10,21 +10,21 @@ impl TokenCipher {
         let mut prost_encoded = Vec::new();
         prost::Message::encode(src, &mut prost_encoded).expect("Failed to encode");
 
-        let mut iv: [u8; 16] = [0; 16];
-        iv.copy_from_slice(&prost_encoded[..16]);
-
-        // calculate key
         let mut hasher = Sha512::new();
         hasher.update(key);
         let key_hash = hasher.finalize();
         let mut aes_key = [0; 24];
         aes_key.copy_from_slice(&key_hash[..24]);
 
+        let mut iv = vec![0u8; 16];
+        iv.copy_from_slice(&prost_encoded[..16]);
+
         let cipher = Cipher::new_192(&aes_key);
-        let encrypted = cipher.cbc_encrypt(&iv, &prost_encoded);
-        let mut data: Vec<u8> = vec![0; iv.len() + encrypted.len()];
-        data[..16].copy_from_slice(&iv);
-        data[16..].copy_from_slice(&encrypted);
+        let mut encrypted = cipher.cbc_encrypt(&iv, &prost_encoded);
+
+        let mut data: Vec<u8> = Vec::with_capacity(iv.len() + encrypted.len());
+        data.append(&mut iv);
+        data.append(&mut encrypted);
 
         let base64_encoded = &general_purpose::STANDARD.encode(data);
 
@@ -32,15 +32,13 @@ impl TokenCipher {
     }
 
     pub fn decrypt<T: Message + Default>(src: &str, key: &str) -> Result<T, String> {
-        let decoded_token = &general_purpose::STANDARD.decode(src);
+        let base64_decoded = &general_purpose::STANDARD.decode(src);
 
-        if let Err(err) = decoded_token {
+        if let Err(err) = base64_decoded {
             return Err(format!("{}", err));
         }
 
-        let decoded_token = decoded_token.as_ref().unwrap();
-        let mut iv: [u8; 16] = [0; 16];
-        iv.copy_from_slice(&decoded_token[..16]);
+        let base64_decoded = base64_decoded.as_ref().unwrap();
 
         let mut hasher = Sha512::new();
         hasher.update(key);
@@ -48,12 +46,15 @@ impl TokenCipher {
         let mut aes_key = [0; 24];
         aes_key.copy_from_slice(&key_hash[..24]);
 
+        let mut iv = vec![0u8; 16];
+        iv.copy_from_slice(&base64_decoded[..16]);
+
         let cipher = Cipher::new_192(&aes_key);
-        let decrypted = cipher.cbc_decrypt(&iv, &decoded_token[16..]);
+        let decrypted = cipher.cbc_decrypt(&iv, &base64_decoded[16..]);
 
-        let result: Result<T, prost::DecodeError> = prost::Message::decode(&decrypted[..]);
+        let prost_decoded: Result<T, prost::DecodeError> = prost::Message::decode(&decrypted[..]);
 
-        match result {
+        match prost_decoded {
             Err(err) => return Err(format!("{}", err)),
             Ok(data) => return Ok(data),
         }
