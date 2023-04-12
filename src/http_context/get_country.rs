bbd00f43023803e5270813b6778a6244f2eed498
Maxim Gorin <maksim.g@simple.app>
2023-04-12 15:52:47 +0300

    Add ip country

--- src/http_context/get_country.rs
+++ src/http_context/get_country.rs
@@ -1,6 +1,7 @@
 use my_http_server::{HttpContext, HttpFailResult};
 
-const HEADER: &str = "cf-ipcountry";
+const HEADER_1: &str = "cf-ipcountry";
+const HEADER_2: &str = "http_cf_ipcountry";
 
 pub trait GetCountry {
     fn get_country_alpha2(&self) -> Result<String, HttpFailResult>;
@@ -8,13 +9,19 @@ pub trait GetCountry {
 
 impl GetCountry for HttpContext {
     fn get_country_alpha2(&self) -> Result<String, HttpFailResult> {
-        if let Some(header_value) = self.request.get_header(HEADER) {
+        if let Some(header_value) = self.request.get_header(HEADER_1) {
+            if let Ok(parsed_header_value) = std::str::from_utf8(header_value.as_bytes()) {
+                return Ok(parsed_header_value.to_owned());
+            }
+        }
+
+        if let Some(header_value) = self.request.get_header(HEADER_2) {
             if let Ok(parsed_header_value) = std::str::from_utf8(header_value.as_bytes()) {
                 return Ok(parsed_header_value.to_owned());
             }
         }
         
-        println!("Can't get country. Header cf-ipcountry not found");
+        println!("Can't get country. Not found header {} nor {}", HEADER_1, HEADER_2);
 
         return Ok("".to_string());
     }
