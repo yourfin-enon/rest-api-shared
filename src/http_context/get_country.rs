a0a7783571dd2b2d409c9fafe6b3aadde37883f4
Maxim Gorin <maksim.g@simple.app>
2023-05-16 12:20:34 +0300

    Refactoring

--- src/http_context/get_country.rs
+++ src/http_context/get_country.rs
@@ -1,7 +1,9 @@
 use my_http_server::{HttpContext, HttpFailResult};
 
-const HEADER_1: &str = "cf-ipcountry";
-const HEADER_2: &str = "http_cf_ipcountry";
+const COUNTRY_HEADERS: [&str; 2] = [
+    "cf-ipcountry",
+    "http_cf_ipcountry",
+];
 
 pub trait GetCountry {
     fn get_country_alpha2(&self) -> Result<String, HttpFailResult>;
@@ -9,19 +11,15 @@ pub trait GetCountry {
 
 impl GetCountry for HttpContext {
     fn get_country_alpha2(&self) -> Result<String, HttpFailResult> {
-        if let Some(header_value) = self.request.get_header(HEADER_1) {
-            if let Ok(parsed_header_value) = std::str::from_utf8(header_value.as_bytes()) {
-                return Ok(parsed_header_value.to_owned());
+        for header in COUNTRY_HEADERS {
+            if let Some(header_value) = self.request.get_header(header) {
+                if let Ok(parsed_header_value) = std::str::from_utf8(header_value.as_bytes()) {
+                    return Ok(parsed_header_value.to_owned());
+                }
             }
         }
 
-        if let Some(header_value) = self.request.get_header(HEADER_2) {
-            if let Ok(parsed_header_value) = std::str::from_utf8(header_value.as_bytes()) {
-                return Ok(parsed_header_value.to_owned());
-            }
-        }
-        
-        println!("Can't get country. Non of the header found: {}, {}", HEADER_1, HEADER_2);
+        println!("Can't get country. Non of the header found:  {:?}", COUNTRY_HEADERS);
 
         return Ok("".to_string());
     }
