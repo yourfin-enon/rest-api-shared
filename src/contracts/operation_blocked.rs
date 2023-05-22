9f287f194322d2347777f6ec40d79981a1c33b2a
Maxim Gorin <maksim.g@simple.app>
2023-05-22 11:57:40 +0300

    Change operation blocked

--- src/contracts/operation_blocked.rs
+++ src/contracts/operation_blocked.rs
@@ -9,16 +9,22 @@ pub struct OperationBlockedApiResponse {
     pub result: ApiResultStatus,
     #[serde(rename = "description")]
     pub description: String,
+    #[serde(rename = "data")]
+    pub data: OperationBlockedApiData,
+}
+
+#[derive(Serialize, Deserialize, Debug, MyHttpObjectStructure)]
+pub struct OperationBlockedApiData {
     #[serde(rename = "expireDate")]
     pub expire_date: i64,
 }
 
 impl OperationBlockedApiResponse {
-    pub fn new(description: String, expire_date: DateTimeAsMicroseconds) -> Self {
+    pub fn new(expire_date: DateTimeAsMicroseconds) -> Self {
         Self {
             result: ApiResultStatus::OperationBlocked,
-            description,
-            expire_date: expire_date.unix_microseconds / 1000,
+            description: "Operation blocked".to_string(),
+            data: OperationBlockedApiData { expire_date: expire_date.unix_microseconds / 1000 },
         }
     }
 }
