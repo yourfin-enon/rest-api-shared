68c7a0918397a5de9c52d38ddef81805af0d20a5
Maxim Gorin <maksim.g@simple.app>
2023-09-23 15:21:11 +0300

    Fix parse lang

--- src/http_context/get_lang.rs
+++ src/http_context/get_lang.rs
@@ -12,20 +12,86 @@ impl GetLang for HttpContext {
     fn get_lang(&self) -> Result<String, HttpFailResult> {
         if let Some(header_value) = self.request.get_key_value(LANG_HEADER) {
             if let Ok(parsed_header_value) = std::str::from_utf8(header_value) {
-                let split: Vec<&str> = parsed_header_value.split(",").collect();
+                let lang = parse_lang(parsed_header_value);
 
-                if let Some(main_lang) = split.first() {
-                    if let Some(main_lang) = Language::from_639_1(main_lang) {
-                        let main_lang = main_lang.to_639_3().to_uppercase();
-                        
-                        return Ok(main_lang);
-                    }
-                }
+                let Some(lang) = lang else {
+                    println!(
+                        "Can't get LANG. Failed to parse value {}. Using default",
+                        parsed_header_value
+                    );
+
+                    return Ok(DEFAULT_LANG.to_string());
+                };
+
+                return Ok(lang);
             }
         }
 
-        println!("Can't get LANG. Header not found {}. Using default", LANG_HEADER);
+        println!(
+            "Can't get LANG. Header not found {}. Using default",
+            LANG_HEADER
+        );
 
-        return Ok(DEFAULT_LANG.to_string());
+        Ok(DEFAULT_LANG.to_string())
+    }
+}
+
+fn parse_lang(src: &str) -> Option<String> {
+    let splits: Vec<&str> = src.split(',').collect();
+    let mut first_split = splits.first();
+
+    let Some(lang) = first_split else {
+        return None;
+    };
+
+    let lang = if lang.contains('-') {
+        let splits: Vec<&str> = lang.split('-').collect();
+        first_split = splits.first();
+
+        let Some(lang) = first_split else {
+            return None;
+        };
+
+        let Some(lang) = Language::from_639_1(lang) else {
+            return None;
+        };
+
+        lang
+    } else {
+        let Some(lang) = Language::from_639_1(lang) else {
+            return None;
+        };
+
+        lang
+    };
+
+    let lang = lang.to_639_3().to_uppercase();
+
+    Some(lang)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::http_context::get_lang::parse_lang;
+
+    #[test]
+    fn parse_ukr_from_long_value() {
+        let lang = parse_lang("uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7");
+
+        assert_eq!("UKR", lang.unwrap());
+    }
+
+    #[test]
+    fn parse_ukr_from_short_value() {
+        let lang = parse_lang("uk");
+
+        assert_eq!("UKR", lang.unwrap());
+    }
+
+    #[test]
+    fn parse_esp_from_short_value() {
+        let lang = parse_lang("es");
+
+        assert_eq!("SPA", lang.unwrap());
     }
 }
